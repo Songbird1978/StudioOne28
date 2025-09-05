db4eb1b566342435f93995ffb44f119f0bc1154f
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-09-05 21:14:47 +0100

    added config files for database.ts and plugins.ts and .env.example

--- server/config/plugins.ts
+++ server/config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: env('NODE_ENV') === 'production'
+        ? '@strapi/provider-upload-cloudinary'
+        : '@strapi/provider-upload-local',
+      providerOptions: env('NODE_ENV') === 'production'
+        ? {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          }
+        : {
+            // Local uploads stored in ./public/uploads
+            sizeLimit: 10000000, // 10MB limit
+          },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
