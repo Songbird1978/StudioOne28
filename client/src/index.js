f216abf0af6bff2250897b4b152994ec266fd6ee
Songbird1978 <118288349+Songbird1978@users.noreply.github.com>
2025-09-09 04:08:06 +0100

    removed future flags from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,12 +8,7 @@ import "normalize.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <BrowserRouter
-            future={{
-                v7_relativeSplatPath: true,
-                v7_startTransition: true,
-            }}
-        >
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
